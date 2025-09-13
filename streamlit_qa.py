9b66068cfeded5f2791fd4d13b6472f0457e675f
offx-krisjay <161944058+offx-krisjay@users.noreply.github.com>
2025-09-13 10:03:54 +0530

    Update streamlit_qa.py

--- streamlit_qa.py
+++ streamlit_qa.py
@@ -6,7 +6,7 @@ from sentence_transformers import SentenceTransformer
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os 
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from datetime import datatime 
+from datetime import datetime 
 
 
 st.set_page_config(page_title="RAG-based QA bot",layout="wide" )
